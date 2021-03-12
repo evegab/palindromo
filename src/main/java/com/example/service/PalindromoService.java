3687c20cb46eeec37f274a5ee17a727d0009758c
Edinson <evegab@unal.edu.co>
2021-03-11 19:01:02 -0500

    optimization with search over tree BFS method

--- src/main/java/com/example/service/PalindromoService.java
+++ src/main/java/com/example/service/PalindromoService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.ArrayList;
+
 public class PalindromoService {
 	public static String getLargetsPalindrome(String string) {
 		if(string==null) {
@@ -14,20 +16,37 @@ public class PalindromoService {
 		String invertida = new StringBuilder(str).reverse().toString();
 		return invertida.equals(str);
 	}
-	private static String subString(String verify) {
+	public static String subString(String verify) {
 		String auxiliar = "";
-		for(int i = 0; i < verify.length(); i++) {
-			for(int j = i + 2; j <= verify.length(); j++) {
-				//System.out.println(verify.substring(i, j));
-				if (isPalindrome(verify.substring(i, j))) {
-					if(verify.substring(i, j).length() > auxiliar.length()) {
-						auxiliar = verify.substring(i, j);
-					}
-				}
-
-			} 
+		ArrayList<String> tree = new ArrayList<>();
+		tree.add(verify);
+		
+		for(int i = 0; i < tree.size(); i++) {
+			if(isPalindrome(tree.get(i))) {
+					auxiliar = tree.get(i);
+					break;
+			}else {
+				generateChild(tree, tree.get(i));
+			}
 		}
-		System.out.println("auxiliar: " + auxiliar);
+		System.out.println("palindromo es: " + auxiliar);
 		return auxiliar;
+	}
+	private static void generateChild(ArrayList<String> list, String parent) {
+		if(parent.length() >= 2){
+		String leftChild = parent.substring(0, parent.length()-1);
+		String rightChild = parent.substring(1);
+		if(!itExist(list, leftChild))
+			list.add(leftChild);
+		if(!itExist(list, rightChild))
+			list.add(rightChild);
+		}
+	}
+	public static boolean itExist(ArrayList<String> tree, String child ) {
+		for(int i = 0; i < tree.size(); i++) {
+			if(tree.get(i).equals(child))
+				return true;
+		}
+		return false;
 	}
 }
